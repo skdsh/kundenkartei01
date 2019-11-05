14ea3b4bd57058694d593164d76bd4e490cc5bb8
Jack <jack.louis@gmx.de>
2019-11-05 22:52:29 +0100

    Initial commit

---
+++ server.js
@@ -0,0 +1,43 @@
+const express = require("express");
+const app = express();
+const fs = require("fs");
+const bcrypt = require("bcrypt");
+const users = [];
+var myCss = {
+  style: fs.readFileSync("./index.css", "utf8")
+};
+app.set("view-engine", "ejs");
+app.use(express.urlencoded({ extended: false }));
+app.get("/", (req, res) => {
+  res.render("index.ejs", { name: "kyle", title: "My Site", myCss: myCss });
+});
+app.post("/", (req, res) => {
+  req.body.login;
+});
+
+app.get("/register", (req, res) => {
+  res.render("register.ejs");
+});
+app.post("/register", async (req, res) => {
+  try {
+    const hashedPassword = await bcrypt.hash(require.body.passport, 10);
+    users.push({
+      id: Date.now().toString(),
+      name: req.body.name,
+      email: require.body.email,
+      password: hashedPassword
+    });
+    res.redirect("/login");
+  } catch {
+    res.redirect("/register");
+    console.log(users);
+  }
+});
+app.get("/leon", (req, res) => {
+  res.render("leon.ejs");
+});
+
+app.get("/login", (req, res) => {
+  res.render("login.ejs");
+});
+app.listen(3000);
